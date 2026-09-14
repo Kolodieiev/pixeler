b2d1581e445902abdcfc97ab0aeea0cd52329a1f
Kolodieiev <valerij.kolodeev@gmail.com>
2026-09-14 11:07:22 +0300

    fix: покращено алгоритм зчитування напруги акумулятора

--- src/util/batt_util.cpp
+++ src/util/batt_util.cpp
@@ -6,17 +6,13 @@ namespace pixeler
 
   float readBattVoltage()
   {
-    float bat_voltage = 0.0f;
+    uint32_t bat_mv = 0;
 
-    for (uint8_t i{0}; i < VOLTAGE_SAMP_NUM; ++i)
-      bat_voltage += analogRead(PIN_VOLT_MEASH);  // TODO analogReadMilliVolts
+    for (uint8_t i = 0; i < VOLTAGE_SAMP_NUM; ++i)
+      bat_mv += analogReadMilliVolts(PIN_VOLT_MEASH);
 
-    bat_voltage /= VOLTAGE_SAMP_NUM;
-    bat_voltage *= 3.3;
-    bat_voltage /= 4095;
-    bat_voltage /= R_DIV_K;
-
-    return bat_voltage;
+    float avg_mv = (float)bat_mv / VOLTAGE_SAMP_NUM;
+    return (avg_mv / 1000.0f) / R_DIV_K;
   }
 
 #endif  // HAS_BATTERY
